9833ca6192c7cb3cc03f829ea48cfa1c401f606a
Benjamin Le Cadre <benjamin.lecadre@gmail.com>
2012-04-24 14:52:53 +0200

    Fichier main.cpp de test de la librairie OpenCV

---
+++ src/main.cpp
@@ -0,0 +1,25 @@
+#include <cv.h>
+#include <cxcore.h>
+#include <highgui.h>
+
+int main(int argc, char* argv[]) {
+		// Open the file.
+		IplImage *img = cvLoadImage("photo.jpg");
+		if (!img) {
+				printf("Error: Couldn't open the image file.\n");
+				return 1;
+		}
+
+		// Display the image.
+		cvNamedWindow("Image:", CV_WINDOW_FULLSCREEN);
+		cvShowImage("Image:", img);
+
+		// Wait for the user to press a key in the GUI window.
+		cvWaitKey(0);
+
+		// Free the resources.
+		cvDestroyWindow("Image:");
+		cvReleaseImage(&img);
+        
+		return 0;
+}
